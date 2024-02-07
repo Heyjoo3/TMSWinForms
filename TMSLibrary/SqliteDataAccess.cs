05c5eaab0c1bc8a1fab8bdc5414d890f7e240a86
frieda <heyjoo.3@gmail.com>
2024-02-07 15:42:25 +0100

    adding sql request methods in SqliteDataAccess

--- TMSLibrary/SqliteDataAccess.cs
+++ TMSLibrary/SqliteDataAccess.cs
@@ -13,6 +13,12 @@ namespace TMSLibrary
 {
     public class SqliteDataAccess
     {
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
         public static List<UserModel> LoadUsers() 
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
@@ -30,12 +36,69 @@ namespace TMSLibrary
             }
         }
 
-        private static string LoadConnectionString(string id = "Default") 
+        public static void UpdateUser(UserModel user)
         {
-            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("update UserTable set Name = @Name, Email = @Email, Password = @Password, Roll = @Roll where Id = @Id", user);
+            }
         }
 
+        public static void DeleteUser(int id)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("delete from UserTable where Id = @Id", new { Id = id });
+            }
+        }
 
+        public static List<TicketModel> LoadTickets()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<TicketModel>("select * from TicketTable", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void SaveTicket(TicketModel ticket)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into TicketTable (Title, Description, Status, Priority, DueDate, AssignedUserID, AssignedUserName) values (@Title, @Description, @Status, @Priority, @DueDate, @AssignedUserID, @AssignedUserName)", ticket);
+            }
+        }
+
+        public static void UpdateTicket(TicketModel ticket)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("update TicketTable set Title = @Title, Description = @Description, Status = @Status, Priority = @Priority, DueDate = @DueDate, AssignedUserID = @AssignedUserID, AssignedUserName = @AssignedUserName where Id = @Id", ticket);
+            }
+        }
+
+        public static void DeleteTicket(int id)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("delete from TicketTable where Id = @Id", new { Id = id });
+            }
+        }
+
+        public static void AssignTicket(int ticketId, int userId, string userName)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("update TicketTable set AssignedUserID = @AssignedUserID, AssignedUserName = @AssignedUserName where Id = @Id", new { AssignedUserID = userId, AssignedUserName = userName, Id = ticketId });
+            }
+        }
+
+        public static void ChangeTicketStatus(int ticketId, int status)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("update TicketTable set Status = @Status where Id = @Id", new { Status = status, Id = ticketId });
+            }
+        }
     }
-
 }
---
+++ TMSLibrary/TicketModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSLibrary
+{
+    public class TicketModel
+    {
+
+        // fields 
+        private int id;
+        private string title;
+        private string description;
+        private int status;
+        private int priority;
+        private string dueDate;
+        private int assignedUserID;
+        private string assignedUserName;
+
+        // properties
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public int Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+            
+        public int Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+        public string DueDate
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+        public int AssignedUserID
+        {
+            get { return assignedUserID; }
+            set { assignedUserID = value; }
+        }
+        public string AssignedUserName
+        {
+            get { return assignedUserName; }
+            set { assignedUserName = value; }
+        } 
+    }
+}
